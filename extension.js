dd808401b9d26dd08a26bf9dae1d3ef9386352a4
Sidney Hays <sidneyh@solution-source.net>
2022-05-04 13:37:28 -0400

    First attempt at File Comparison.

--- extension.js
+++ extension.js
@@ -1,30 +1,77 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const compareFileWithFileCabinetBeginMessage = 'Downloading file from NetSuite...';
+
+const suiteCloudImportSuccessPattern = 'The following files were imported:';
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	context.subscriptions.push(vscode.commands.registerCommand('unofficial-suitecloud-extras.compareFileWithFileCabinet', () => {compareFileWithFileCabinet(context);}));
+}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "unofficial-suitecloud-extras" is now active!');
+function compareFileWithFileCabinet (context) {
+	// TODO Add a setting for showing the Terminal.
+	// TODO If said setting is hiding the terminal, then we'll show the following.
+	vscode.window.showInformationMessage(compareFileWithFileCabinetBeginMessage);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('unofficial-suitecloud-extras.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	vscode.window.activeTextEditor.document.save().then(() => {
+		let currentFileUri = vscode.window.activeTextEditor.document.uri;
+		let currentFilePath = currentFileUri.path;
+		let currentFileExtension = path.extname(currentFilePath);
+		let tempFilePath = currentFilePath.replace(currentFileExtension, '_' + Date.now() + currentFileExtension);
+		let tempFileUri = vscode.Uri.file(tempFilePath);
+		let fileCabinetFilePath = currentFilePath.split('/FileCabinet').pop();
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Unofficial SuiteCloud Extras!');
+		vscode.workspace.fs.copy(currentFileUri, tempFileUri).then(() => {
+			const tempOnDidCloseTextDocument = vscode.workspace.onDidCloseTextDocument((textDocument) => {
+				if (textDocument.uri.path == tempFileUri.path) {
+					vscode.workspace.fs.delete(textDocument.uri);
+
+					// We want this event handler to dispose of itself.
+					tempOnDidCloseTextDocument.dispose();
+				}
+			});
+			// Add the disposable just in case it never fires
+			context.subscriptions.push(tempOnDidCloseTextDocument);
+
+			let terminal = vscode.window.createTerminal({
+				name: 'FileCabinet Compare...',
+				message: compareFileWithFileCabinetBeginMessage + '\n',
+				// TODO NoLogo doesn't seem to be working here for some reason.
+				shellArgs: ['-NoLogo'],
+				isTransient: true
+			});
+
+			// TODO Add a setting for showing the Terminal.
+			terminal.show(true);
+
+			const suiteCloudCommand = `suitecloud file:import --paths "${fileCabinetFilePath}" --excludeproperties`;
+			const errorCheckCommand = `if ($__ | Select-String -Pattern "${suiteCloudImportSuccessPattern}" -quiet) {exit 0} else {pause; exit 1}`;
+			const finalCommand = `clear; ${suiteCloudCommand} | Write-Output -OutVariable __; ${errorCheckCommand}`;
+
+			terminal.sendText(finalCommand);
+
+			const tempOnDidCloseTerminal = vscode.window.onDidCloseTerminal((closedTerminal) => {
+				if (terminal == closedTerminal) {
+					console.log(closedTerminal.exitStatus);
+					if (closedTerminal.exitStatus && closedTerminal.exitStatus.code)
+					vscode.window.showErrorMessage('Something went wrong when trying to download the file. Exit Code: ' + terminal.exitStatus.code);
+					else
+					vscode.commands.executeCommand("vscode.diff", currentFileUri, tempFileUri, undefined, {preview: true});
+					
+					// We want this event handler to dispose of itself.
+					tempOnDidCloseTerminal.dispose();
+				}
+			});
+			// Add the disposable just in case it never fires
+			context.subscriptions.push(tempOnDidCloseTerminal);
+		});
 	});
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
